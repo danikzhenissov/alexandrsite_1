88e231948a2faa76c8284dc8570b6d6f6a0aca6c
Anoir <anoir@pop-os.localdomain>
2021-08-20 02:37:39 +0600

    service slider done

---
+++ js/serviceSlider.js
@@ -0,0 +1,23 @@
+const serviceRadio = document.getElementById("service-radio");
+const serviceSlider = document.getElementById("service-slider");
+
+const serviceInit = () => {
+    selectServiceRadio(serviceRadio.children[0], 0);
+    for (let i = 0; i < serviceRadio.children.length; i++) {
+        let element = serviceRadio.children[i];
+        element.addEventListener('click', () => selectServiceRadio(element, i));
+    }
+}
+
+const selectServiceRadio = (element, id)  =>{
+    for (let i = 0; i < serviceRadio.children.length; i++) {
+        let radio = serviceRadio.children[i];
+        let slider = serviceSlider.children[i];
+        radio.classList.remove('active');
+        slider.classList.replace('block','hidden');
+    }
+    element.classList.add("active");
+    serviceSlider.children[id].classList.replace("hidden", "block");
+}
+
+serviceInit();
